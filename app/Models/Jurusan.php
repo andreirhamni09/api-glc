d6f0eff0eff04a87ca96cddd8d01f4fb9d41fac0
andreirhamni09 <andreirhamni09@gmail.com>
2022-03-02 01:55:46 +0700

    Update API

--- app/Models/Jurusan.php
+++ app/Models/Jurusan.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Jurusan extends Model
 {
     use HasFactory;
+    
+    public $primaryKey  = 'id';
+    protected $fillable = [
+        'id', 'jurusan', 'updated_at'
+    ];
+
+    static function getAllJurusan(){
+        $jurusan = DB::table('jurusans')->get();
+        return $jurusan;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\KaryawanController;
+use App\Http\Controllers\API\JurusanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+# ~~ Admin
+    # ~~ Karyawan 
+        Route::resource('karyawan-admin', KaryawanController::class);
+    # ~~
+
+    # ~~ Jurusan
+        Route::resource('jurusan', JurusanController::class);
+    # ~~
+# ~~
