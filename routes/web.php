39211c13e153361379d318ca3275bf2543c48c7a
andreirhamni09 <andreirhamni09@gmail.com>
2022-03-15 14:26:54 +0700

    Tambahkan Direktori

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+# ~~ Peserta Didik
+ Route::prefix('/')->group(function(){
+    Route::get('', function(){
+        return view('test');
+    });
 });
+# ~~
+
+
 
 Route::get('/test', function(){
     return view('test');
